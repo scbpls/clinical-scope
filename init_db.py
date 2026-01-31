895bb5a1278718350cdd94acc243a36e4f58ee39
jakub.marciniak <jakub.marciniak@notoria.pl>
2026-01-31 04:56:22 +0100

    Add CSV export

--- init_db.py
+++ init_db.py
@@ -11,11 +11,11 @@ full_df = pd.concat([df1, df2, df3])
 
 # Usunięcie diplikatów
 full_df = full_df.drop_duplicates(subset=["NCT Number"])
-print(f"Liczba unikalnych badań: {len(full_df)}")
 
 # Zapisanie do bazy danych
+print("Zapisywanie danych do bazy...")
 conn = sqlite3.connect("clinical_trials.db")
 full_df.to_sql("trials", conn, if_exists="replace", index=False)
 conn.close()
 
-print("Baza danych gotowa")
+print("Baza danych gotowa!")
